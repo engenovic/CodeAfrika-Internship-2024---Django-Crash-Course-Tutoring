6aa177c260977ce2ed9dd19fa6ee994d95cc0fb7
engenovic <engenovic@gmail.com>
2024-07-29 22:39:00 +0300

    Installed Django 5

---
+++ D5DaysPrepLib/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for D5DaysPrepLib project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'D5DaysPrepLib.settings')
+
+application = get_asgi_application()
